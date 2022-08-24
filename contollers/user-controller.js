be64173188439ca27ebc3e6da4eb360502c93b74
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-08-24 17:27:52 -0400

    created GET and POST routes for users

--- contollers/user-controller.js
+++ contollers/user-controller.js
@@ -2,7 +2,7 @@ const { User } = require("../models");
 
 const userController = {
   // GET all users
-  getAllUser(req, res) {
+  getAllUsers(req, res) {
     User.find({})
       .populate({
         path: "thoughts",
@@ -17,12 +17,36 @@ const userController = {
       });
   },
   // GET a single user by its _id and populated thought and friend data
-  //
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.userId })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
   // POST a new user:
   // example data
   // {"username": "lernantino",
   // "email": "lernantino@gmail.com"}
-  //
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(400).json(err));
+  },
+
   // PUT to update a user by its _id
   // DELETE to remove user by its _id
   // Remove a user's associated thoughts when deleted.
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 
-const { getAllUser } = require("../../contollers/user-controller");
+const { getAllUsers, getUserById, createUser } = require("../../contollers/user-controller");
 
-router.route("/").get(getAllUser);
+router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:userId").get(getUserById);
+
+router.route("/:userId/friends/:friendId");
+
 module.exports = router;
